2199eb5ede21415f904c36c36c985949c00b974a
SujalDesale <sujaldesale1700@gmail.com>
2026-05-01 23:00:00 +0530

    fix mongodb connection for production

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -19,10 +22,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const JWT_SECRET = "secret123";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // ================= DB =================
-mongoose.connect("mongodb://127.0.0.1:27017/taskflow")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log(err));
 
